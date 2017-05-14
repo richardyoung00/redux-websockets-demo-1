602f1c9fbed8ca2fdfed3169f88c921a2f61d91e
Richard Young <richard@bnry.local>
2017-05-14 20:17:47 +0200

    Add comments to reducer

--- src/reducers/files.js
+++ src/reducers/files.js
@@ -6,6 +6,7 @@ const initialState = {
 const files = (state = initialState, action) => {
 
   switch (action.type) {
+    // file was added to the server
     case 'SERVER_FILE_ADDED':
       const addList = state.fileList.slice();
       addList.push(action.filename);
@@ -13,11 +14,13 @@ const files = (state = initialState, action) => {
         ...state,
         fileList: addList
       };
+    // file was removed from the server
     case 'SERVER_FILE_REMOVED':
       return {
         ...state,
         fileList: removeItem(state.fileList, action.filename)
       };
+    // clear all files
     case 'CLEAR_FILES':
       return {
         ...state,
@@ -28,6 +31,7 @@ const files = (state = initialState, action) => {
   }
 };
 
+// remove an item from an array
 function removeItem(array, name) {
   return array.filter( (item, index) => item !== name);
 }
